e5546d6edc0aae7eb737b7437b7bcbeed4589bc2
61003 <g100m8888@gmail.com>
2021-12-25 08:49:13 +0400

    create user api

---
+++ todo/users/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+from .models import User
+
+
+class UserModelSerializer(HyperlinkedModelSerializer):
+   class Meta:
+       model = User
+       fields = ['uuid','username', 'firstname', 'lastname', 'email']
