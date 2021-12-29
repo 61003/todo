2431035126c1058235b72bd39530d79e3e8ac7e6
61003 <g100m8888@gmail.com>
2021-12-29 19:00:52 +0400

    create todo frontend

---
+++ todo/frontend/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import UserList from './components/User.js'
+import AppMenu from './components/Menu.js'
+import AppFooter from './components/Footer.js'
+import axios from 'axios'
+
+class App extends React.Component {
+
+   constructor(props) {
+       super(props)
+       this.state = {
+           'users': []
+       }
+   }
+
+   componentDidMount() {
+   axios.get('http://127.0.0.1:8000/api/users')
+       .then(response => {
+           const users = response.data
+               this.setState(
+               {
+                   'users': users
+               }
+           )
+       }).catch(error => console.log(error))
+   }
+
+   render () {
+       return (
+           <div>
+             <AppMenu />
+             <UserList users={this.state.users} />
+             <AppFooter />
+           </div>
+       )
+   }
+}
+
+
+export default App;
