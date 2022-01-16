1420eaecc7118e2aa4ad02d4db38f22e6ca389cc
61003 <g100m8888@gmail.com>
2022-01-16 18:20:15 +0400

    Lesson 2 (#2)
    * create user api
    
    * create default admin function
    
    * create library frontend
    
    * create todo frontend

---
+++ todo/frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const AppFooter = () => {
+   return (
+       <h2>My footer</h2>
+   )
+}
+
+export default AppFooter
---
+++ todo/frontend/src/components/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const AppMenu = () => {
+   return (
+       <h1>My menu</h1>
+   )
+}
+
+export default AppMenu
