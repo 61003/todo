66f77edf471f7a9389869d88705881ee9f8bb9ea
61003 <g100m8888@gmail.com>
2022-01-25 07:38:49 +0400

    lesson 6

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserModelViewSet
 from work.views import ProjectModelViewSet, TodoModelViewSet
+from rest_framework.authtoken import views
+
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
@@ -28,4 +30,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+
 ]
